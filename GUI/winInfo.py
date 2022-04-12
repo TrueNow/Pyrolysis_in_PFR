18f29dedd53d2378b4657acb7dc633a134137e3e
s3r3ga <sergeytru15@gmail.com>
2022-04-12 23:02:07 +1000

    Edit windows about info and errors.

--- GUI/winInfo.py
+++ GUI/winInfo.py
@@ -1,24 +1,16 @@
-import PySimpleGUI as sg
+from tkinter import messagebox
 
 
-class WinInfo:
-    def __init__(self, main):
-        self.__main = main
+class Error:
+    @staticmethod
+    def error(title: str, message: str):
+        messagebox.showerror(title=title, message=message)
 
-    def open(self):
-        window_info = sg.Window('Выполнено',
-                                layout=[
-                                    [sg.Text('Расчет завершен')],
-                                    [sg.Button('Закрыть', key='--INFO--')]
-                                ],
-                                background_color='WHITE')
+    @staticmethod
+    def error_and_function(title: str, message: str, function):
+        messagebox.showerror(title=title, message=message)
+        function()
 
-        while True:
-            event, values = window_info.read()
-
-            if event == sg.WIN_CLOSED:
-                break
-
-            if event == '--INFO--':
-                window_info.close()
-                break
+    @staticmethod
+    def info(title: str, message: str):
+        messagebox.showinfo(title=title, message=message)
