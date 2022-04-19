254558ccf24710c01af9469763b62649e108c615
s3r3ga <sergeytru15@gmail.com>
2022-04-20 00:45:53 +1000

    Updating saving results to xlsx file.

--- src/Model.py
+++ src/Model.py
@@ -4,39 +4,44 @@ from src.Saver import Saver
 
 class Model:
     def __init__(self, reactions, components, cascade):
-        self.__reactions = reactions
-        self.__components = components
-        self.__cascade = cascade
-        self.__section = None
-        save = Saver(self, 'example.xlsx')
+        self._reactions = reactions
+        self._components = components
+        self._cascade = cascade
+        self._section = None
+        save = Saver(self)
 
-        for number, reactor in self.__cascade.get_cascade().items():
-            save.init_in_xlsx(number)
+        molar_flow_out = None
+
+        start_row = 1
+        for name, reactor in self._cascade.get_cascade().items():
+            if molar_flow_out is not None:
+                self._cascade.get_reactor(name).molar_flow_in = molar_flow_out
+            self._components.calculate_fractions(self._cascade.get_reactor(name).molar_flow_in)
+            save.init_in_xlsx(start_row)
             self.calculate(reactor)
-            save.reactor_in_xlsx(number)
-            save.result_in_xlsx(number)
+            save.reactor_in_xlsx(reactor)
+            self._components.calculate_fractions(self.get_section().molar_flow_out)
+            save.result_in_xlsx()
 
-            try:
-                self.__cascade.get_reactor(number + 1).molar_flow_in = self.get_section().molar_flow_out
-            except KeyError:
-                pass
+            molar_flow_out = self.get_section().molar_flow_out
+            start_row += (len(self._components.get_components()) + 5)
 
     def calculate(self, reactor):
-        self.__section = reactor.create_section()
+        self._section = reactor.create_section()
         self.calculate_section()
 
     def calculate_section(self):
-        while self.__section.next() is True:
+        while self._section.next() is True:
             concentration_inlet = {}
-            for name, component in self.__components.get_components().items():
-                concentration_inlet[name] = self.__section.calc_component_concentration(self, name)
+            for name, component in self._components.get_components().items():
+                concentration_inlet[name] = self._section.calc_component_concentration(self, name)
 
             concentration_outlet = dict.copy(concentration_inlet)
             total_concentration_inlet = sum(concentration_inlet.values())
 
-            temp = self.__section.temp_in
-            tay = self.__section.tay
-            for reaction in self.__reactions.get_reactions().values():
+            temp = self._section.temp_in
+            tay = self._section.tay
+            for reaction in self._reactions.get_reactions().values():
                 result = reaction.calculate(self, temp, tay)
                 for name, delta in result.items():
                     concentration_outlet[name] += delta
@@ -45,27 +50,30 @@ class Model:
                         exit('Ну вот и все...')
 
             total_concentration_outlet = sum(concentration_outlet.values())
-            for name, component in self.__components.get_components().items():
-                component.mol_fr = concentration_outlet[name] / total_concentration_outlet
-            self.__section.molar_flow_out = self.__section.molar_flow_in * total_concentration_outlet / total_concentration_inlet
+            for name, component in self._components.get_components().items():
+                component.ratio = concentration_outlet[name] / total_concentration_outlet
+                component.mol_fr = component.ratio
+            self._section.molar_flow_out = self._section.molar_flow_in * total_concentration_outlet / total_concentration_inlet
 
-            if not self.__section.count % int(str(self.__section.numberOfSections)[:-2]):
-                print(f'{(self.__section.count / int(str(self.__section.numberOfSections)[:-2]))} % {datetime.datetime.now()}')
+        for name, params in self._components.get_components().items():
+            print(name, params.mol_fr)
+            # if not self._section.count % int(str(self._section.numberOfSections)[:-2]):
+            #     print(f'{(self._section.count / int(str(self._section.numberOfSections)[:-2]))} % {datetime.datetime.now()}')
 
     def get_cascade(self):
-        return self.__cascade
+        return self._cascade
 
     def get_section(self):
-        return self.__section
+        return self._section
 
     def get_reactions(self):
-        return self.__reactions
+        return self._reactions
 
     def get_components(self):
-        return self.__components
+        return self._components
 
     def get_result_flow(self):
         result = {}
-        for name, component in self.__components.get_used_components().items():
+        for name, component in self._components.get_components().items():
             result[name] = component.mol_fr
         return result
--- src/Saver.py
+++ src/Saver.py
@@ -7,207 +7,89 @@ class Saver:
     row_add = 0
     start_col = 0
 
-    def __init__(self, model, name):
+    def __init__(self, model):
         self.model = model
-        self.name_file = name
+        self.filename = f'{self.model.get_reactions().filename[:-5]} in {self.model.get_cascade().filename[:-5]}.xlsx'
 
         # Создание Excel файла
         self.xlsx = openpyxl.Workbook()
-        sheet = self.xlsx.active
-        self.xlsx.remove_sheet(sheet)
-        self.xlsx.create_sheet('Результаты')
-        self.sheet = self.xlsx['Результаты']
+        self.sheet = self.xlsx.active
+        self.sheet.title = 'Результаты'
 
-        self.start_row = 0
+    def init_in_xlsx(self, start):
+        self.start_row = start
+        row, column = self.start_row, 1
+        self.save_components(row, column)
 
-    def init_in_xlsx(self, number_of_reactor: int):
-        number = number_of_reactor  # Порядковый номер реактора начиная с 0
-        self.row_add = number * (len(self.model.get_components().get_components()) + 4)
-        self.start_col = 0
-        value = self.model.get_cascade().get_reactor(number).molar_flow_in
-        self.values_in_xlsx(value)
+    def reactor_in_xlsx(self, reactor):
+        row, start_column = self.start_row, 8
+        list_reactor = [
+            [None, reactor.name, None, None, ''],
+            ['Температура', reactor.temp_in, reactor.temp_out, 'C', '0.00'],
+            ['Давление', reactor.press_in, reactor.press_out, 'кПа', '0.00'],
+            ['Объем реактора', reactor.volume, None, 'м3', '0.00'],
+            ['Число секций', reactor.steps, None, 'шт', '0'],
+            ['Время пребывания', reactor.get_section().time_count, None, 'с', '0.000000']
+        ]
 
-    def result_in_xlsx(self, number_of_reactor: int):
-        number = number_of_reactor  # Порядковый номер реактора начиная с 0
-        self.row_add = number * (len(self.model.get_components().get_components()) + 4)
-        self.start_col = 12
-        value = self.model.get_cascade().get_reactor(number).get_section().molar_flow_out
-        self.values_in_xlsx(value)
+        for string in list_reactor:
+            column = start_column
+            for param in string[:-1]:
+                try:
+                    self.sheet.cell(row=row, column=column, value=param).number_format = string[-1]
+                except:
+                    self.sheet.cell(row=row, column=column, value=param)
+                column += 1
+            row += 1
+        self.xlsx.save(f'{self.filename}')
 
-    def values_in_xlsx(self, value: float):
-        self.title()
+    def result_in_xlsx(self):
+        row, column = self.start_row, 13
+        self.save_components(row, column)
 
-        col_name = self.start_col + 1
-        self.name(col_name)
+    def save_components(self, start_row, start_column):
+        row = start_row
+        column = start_column
 
-        col_ratio = self.start_col + 2
-        self.ratio(col_ratio)
+        sum_mol_fr = 0
+        sum_mol = 0
+        sum_mass = 0
+        sum_mass_fr = 0
 
-        col_mol_fr = self.start_col + 3
-        self.mol_fr(col_mol_fr)
+        params = self.rounder('Component', 'MolarMass', 'MolFr', 'Mol', 'Mass', 'MassFr')
+        for param in params.values():
+            self.sheet.cell(row=row, column=column, value=param['value']).number_format = param['rounding']
+            column += 1
 
-        row = self.start_row + 2 + self.row_add  # 2
-        col = self.start_col + 4  # 4
-        self.sheet.cell(row=row, column=col).value = value
+        for name, component in self.model.get_components().get_components().items():
+            row += 1
 
-        col_mol = self.start_col + 4
-        self.mol(col_mol)
+            sum_mol_fr += component.mol_fr
+            sum_mol += component.mol
+            sum_mass += component.mass
+            sum_mass_fr += component.mass_fr
 
-        col_molar_mass = self.start_col + 5
-        self.molar_mass(col_molar_mass)
+            column = start_column
+            params = self.rounder(name, component.molar_mass, component.mol_fr, component.mol, component.mass, component.mass_fr)
+            for param in params.values():
+                self.sheet.cell(row=row, column=column, value=param['value']).number_format = param['rounding']
+                column += 1
 
-        col_mass = self.start_col + 6
-        self.mass(col_mass)
+        column = start_column
+        row += 1
+        params = self.rounder('Сумма', None, sum_mol_fr, sum_mol, sum_mass, sum_mass_fr)
+        for param in params.values():
+            self.sheet.cell(row=row, column=column, value=param['value']).number_format = param['rounding']
+            column += 1
+        self.xlsx.save(f'{self.filename}')
 
-        col_mass_fr = self.start_col + 7
-        self.mass_fr(col_mass_fr)
-
-        self.xlsx.save(f'Result/{self.name_file}')
-
-    def title(self):
-        title = {
-            self.start_col + 1: 'Components',
-            self.start_col + 2: 'Ratio',
-            self.start_col + 3: 'MolFr',
-            self.start_col + 4: 'Mol',
-            self.start_col + 5: 'MolarMass',
-            self.start_col + 6: 'Mass',
-            self.start_col + 7: 'MassFr'
+    @staticmethod
+    def rounder(one, two, three, four, five, six):
+        return {
+            '1': {'value': one, 'rounding': ''},
+            '2': {'value': two, 'rounding': '0.0000'},
+            '3': {'value': three, 'rounding': '0.0000'},
+            '4': {'value': four, 'rounding': '0.00'},
+            '5': {'value': five, 'rounding': '0.00'},
+            '6': {'value': six, 'rounding': '0.0000'},
         }
-
-        row = self.start_row + 1 + self.row_add
-        for col in range(1, len(title) + 1):
-            self.sheet.cell(row=row, column=self.start_col + col).value = title[self.start_col + col]
-
-    def name(self, col: int):
-        row = self.start_row + 3 + self.row_add
-        col_name = col
-        for name in self.model.get_components().get_components().keys():
-            self.sheet.cell(row=row, column=col_name).value = name
-            row += 1
-
-    def ratio(self, col: int):
-        row = self.start_row + 3 + self.row_add
-        col_ratio = col
-        for component in self.model.get_components().get_components().values():
-            self.sheet.cell(row=row, column=col_ratio).value = component.mol_fr
-            self.sheet.cell(row=row, column=col_ratio).number_format = '0.00'
-            row += 1
-        self.sum_in_column(col_ratio)
-
-    def mol_fr(self, col: int):
-        row = self.start_row + 3 + self.row_add
-        col_mol_fr = col
-        col_ratio = col_mol_fr - 1
-        for _ in self.model.get_components().get_components().keys():
-            self.sheet.cell(row=row, column=col_mol_fr).value = self.sheet.cell(row=row, column=col_ratio).value
-            self.sheet.cell(row=row, column=col_mol_fr).number_format = '0.0000'
-            row += 1
-        self.sum_in_column(col_mol_fr)
-
-    def mol(self, col: int):
-        row = self.start_row + 3 + self.row_add
-        col_mol = col
-        col_mol_fr = col_mol - 1
-        molFlow = self.sheet.cell(row=self.start_row + 2 + self.row_add, column=col_mol).value
-        for _ in self.model.get_components().get_components().keys():
-            self.sheet.cell(row=row, column=col_mol).value = self.sheet.cell(row=row, column=col_mol_fr).value * molFlow
-            self.sheet.cell(row=row, column=col_mol).number_format = '0.00'
-            row += 1
-        self.sum_in_column(col_mol)
-
-    def molar_mass(self, col: int):
-        row = self.start_row + 3 + self.row_add
-        col_molar_mass = col
-        for component in self.model.get_components().get_components().values():
-            self.sheet.cell(row=row, column=col_molar_mass).value = component.molar_mass
-            self.sheet.cell(row=row, column=col_molar_mass).number_format = '0.00'
-            row += 1
-
-    def mass(self, col: int):
-        row = self.start_row + 3 + self.row_add
-        col_mass = col
-        col_molar_mass = col_mass - 1
-        col_mol = col_molar_mass - 1
-        for _ in self.model.get_components().get_components().keys():
-            molar_mass = self.sheet.cell(row=row, column=col_molar_mass).value
-            mol = self.sheet.cell(row=row, column=col_mol).value
-            self.sheet.cell(row=row, column=col_mass).value = mol * molar_mass
-            self.sheet.cell(row=row, column=col_mass).number_format = '0.00'
-            row += 1
-        self.sum_in_column(col_mass)
-
-    def mass_fr(self, col: int):
-        row = self.start_row + 3 + self.row_add
-        col_mass_fr = col
-        col_mass = col_mass_fr - 1
-
-        sum_mass = self.sheet.cell(row=self.start_row + 2, column=col_mass).value
-        for _ in self.model.get_components().get_components().keys():
-            mass = self.sheet.cell(row=row, column=col_mass).value
-            self.sheet.cell(row=row, column=col_mass_fr).value = mass / sum_mass
-            self.sheet.cell(row=row, column=col_mass_fr).number_format = '0.0000'
-            row += 1
-        self.sum_in_column(col_mass_fr)
-
-    def sum_in_column(self, col: int):
-        row = self.start_row + 3 + self.row_add
-        column = col
-        s = 0
-        for _ in self.model.get_components().get_components().keys():
-            s += self.sheet.cell(row=row, column=column).value
-            row += 1
-        row = self.start_row + 2 + self.row_add
-        self.sheet.cell(row=row, column=column).value = s
-        self.sheet.cell(row=row, column=column).number_format = '0.00'
-
-    def reactor_in_xlsx(self, i: int):
-        parameters = [0, 1,
-                      self.model.get_cascade().get_reactor(i).name,
-                      self.model.get_cascade().get_reactor(i).temp_in,
-                      self.model.get_cascade().get_reactor(i).temp_out,
-                      self.model.get_cascade().get_reactor(i).press_in,
-                      self.model.get_cascade().get_reactor(i).press_out,
-                      self.model.get_cascade().get_reactor(i).volume,
-                      self.model.get_cascade().get_reactor(i).steps,
-                      self.model.get_cascade().get_reactor(i).get_section().time_count]
-        for row in range(2, len(parameters)):
-            self.sheet.cell(row=self.start_row + row + self.row_add, column=10).value = parameters[row]
-        self.reactor_title()
-
-        self.xlsx.save(f'Result/{self.name_file}')
-
-    def reactor_title(self):
-        title = {
-            'parameters': [0, 1, 2,
-                           'Температура вход', 'Температура выход',
-                           'Давление вход', 'Давление выход',
-                           'Объем реактора', 'Число секций', 'Время пребывания'],
-            'format': [0, 1, 2,
-                       '0.0', '0.0',
-                       '0.0', '0.0',
-                       '0.0', '0', '0.000'],
-            'dimension': [0, 1, 2,
-                          'C', 'C',
-                          'кПа', 'кПа',
-                          'м3', 'шт', 'с'],
-        }
-        for row in range(3, len(title['parameters'])):
-            self.sheet.cell(row=self.start_row + row + self.row_add, column=9).value = title['parameters'][row]
-            self.sheet.cell(row=self.start_row + row + self.row_add, column=10).number_format = title['format'][row]
-            self.sheet.cell(row=self.start_row + row + self.row_add, column=11).value = title['dimension'][row]
-
-    def work(self):
-        row_start = 4
-        result = self.xlsx.active
-        sheet = self.xlsx.create_sheet('Итог')
-        for i in range(row_start, int(4 + len(self.model.get_components().get_used_components()))):
-            if self.model.get_components().get_component(result.cell(row=i, column=1).value).type == 'mol':
-                sheet.cell(row=row_start, column=1).value = result.cell(row=i, column=1).value
-                sheet.cell(row=row_start, column=2).value = result.cell(row=i, column=result.max_column - 3).value
-                sheet.cell(row=row_start, column=2).number_format = '0.000000'
-                sheet.cell(row=row_start, column=3).value = 'мол. доля'
-                row_start += 1
-        sheet.cell(row=sheet.max_row + 2, column=1).value = 'Расход'
-        sheet.cell(row=sheet.max_row, column=2).value = result.cell(row=3, column=result.max_column - 2).value
-        sheet.cell(row=sheet.max_row, column=3).value = 'кмоль/с'
