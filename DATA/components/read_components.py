295c3911691a3aea2fcef6250edcf6815a608b94
s3r3ga <sergeytru15@gmail.com>
2022-04-30 15:09:43 +1000

    Update reader of components from xlsx-file to class.

--- DATA/components/read_components.py
+++ DATA/components/read_components.py
@@ -1,43 +1,24 @@
 import openpyxl
+from src.Components import Components
 
 
 def read_components_from_xlsx(folder='D:/Models_In_Python/Work_Model v4.0/DATA/components',
                               filename='All_components.xlsx'):
-    """
-    components = {
-        component: {
-            'Молярная масса': float,\n
-            'Мольная доля': float,\n
-            'Массовая доля': float,\n
-            'Тип': str,\n
-            'Формула': str\n
-        }
-    }
-
-    :return: All components from xlsx file
-    :rtype: dict
-    """
-
     file = f'{folder}/{filename}'
 
     xlsx = openpyxl.load_workbook(file, data_only=True)
     sheet = xlsx.active
-    components = {}
-    row = 2
-    while True:
-        name = sheet.cell(column=1, row=row).value
-        if name is None:
-            break
-        else:
-            components[name] = {}
-            for col in range(2, sheet.max_column + 1):
-                title_cell = sheet.cell(column=col, row=1)
-                value_cell = sheet.cell(column=col, row=row)
-                try:
-                    components[name][title_cell.value] = float(value_cell.value)
-                except TypeError:
-                    pass
-                except ValueError:
-                    components[name][title_cell.value] = value_cell.value
-        row += 1
-    return components
+
+    components = Components()
+    data = []
+
+    titles = [col[0].value for col in sheet.iter_cols(1, 4)]
+
+    for row in sheet.iter_rows(2):
+        if row[0].value is not None:
+            properties = {}
+            for col in range(len(titles)):
+                properties[titles[col]] = row[col].value
+            components.add_component(properties['name'], properties)
+            data.append([*properties.values()])
+    return components, data
--- DATA/reactions/read_reactions.py
+++ DATA/reactions/read_reactions.py
@@ -1,40 +1,23 @@
 import openpyxl
+from src.Reactions import Reactions
 
 
-def read_reactions_from_xlsx(folder='D:/Models_In_Python/Work_Model v4.0/DATA/reactions',
+def read_reactions_from_xlsx(folder='./DATA/reactions',
                              filename='example.xlsx'):
-    """
-    reactions = {
-        id: {
-            'ID': int,\n
-            'Components': dict,\n
-            'Divider': float,\n
-            'Order': dict,\n
-            'A': float,\n
-            'E': float,\n
-            'n': int,\n
-            'equation': str,\n
-        }
-    }
-
-    :return: All reactions from xlsx file
-    :rtype: dict
-    """
-
     xlsx = openpyxl.load_workbook(f'{folder}/{filename}', data_only=True)
     sheet = xlsx.active
 
-    reactions = {}
+    reactions = Reactions(filename)
 
     row_id = 1
     while True:
         id = sheet.cell(column=1, row=row_id).value
         if id is not None:
-            reactions[id] = {
-                'ID': id,
-                'Components': {},
-                'Divider': 0,
-                'Order': {},
+            parameters = {
+                'id': id,
+                'components': {},
+                'divider': 0,
+                'order': {},
                 'A': 0,
                 'E': 0,
                 'n': 0,
@@ -44,17 +27,20 @@ def read_reactions_from_xlsx(folder='D:/Models_In_Python/Work_Model v4.0/DATA/re
             for col in range(2, 6):
                 name = sheet.cell(column=col, row=row_id).value
                 coefficient = sheet.cell(column=col, row=row_id + 1).value
-                n = sheet.cell(column=col, row=row_id + 2).value
+                order = sheet.cell(column=col, row=row_id + 2).value
                 if name is not None:
-                    reactions[id]['Components'][name] = coefficient
-                    reactions[id]['Order'][name] = n
+                    parameters['components'][name] = coefficient
+                    parameters['order'][name] = order
 
             for col in range(6, 8):
                 parameter = sheet.cell(column=col, row=row_id).value
                 value = sheet.cell(column=col, row=row_id + 2).value
-                reactions[id][parameter] = value
-            reactions[id]['n'] = sum(reactions[id]['Order'].values())
-            reactions[id]['Divider'] = sheet.cell(column=8, row=row_id + 1).value
+                parameters[parameter] = value
+            parameters['n'] = sum(parameters['order'].values())
+            parameters['divider'] = sheet.cell(column=8, row=row_id + 1).value
             row_id += 4
+
+            reactions.add_reaction(id, parameters)
+
         else:
             return reactions
